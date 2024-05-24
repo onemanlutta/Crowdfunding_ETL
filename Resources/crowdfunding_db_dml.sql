61648fa88c0e8d808fc9da9eb8d10515de799058
maerolutta <118937365+onemanlutta@users.noreply.github.com>
2024-05-23 21:48:30 -0500

    adding to the repo with PostgreSQL DDL, DML, dB and Screenshots

---
+++ Resources/crowdfunding_db_dml.sql
@@ -0,0 +1,16 @@
+/***********************************************
+** DATA ANALYTICS
+** File: crowdfunding_db
+** Desc: DML Script for the crowdfunding_db Database
+** Auth: Eunice Mwangi & Maero Lutta
+** Date: 05/23/2024
+** Ref : Crowdfunding ETL Mini Project
+************************************************/
+
+SELECT * FROM category;
+
+SELECT * FROM subcategory;
+
+SELECT * FROM contacts;
+
+SELECT * FROM campaign;
